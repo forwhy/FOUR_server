59f69f1d4a8d3e5db8757387d515d99e06c68bf7
Tanya Neljubina <ahominefan@gmail.com>
2017-11-06 21:42:23 +0300

    Add connection to DBPostgreSQL with Hibernate

--- FOUR_server/src/main/java/FOUR_server/App.java
+++ FOUR_server/src/main/java/FOUR_server/App.java
@@ -1,10 +1,16 @@
 package FOUR_server;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        sessionFactory.close();
+        System.out.println( "Bye World!" );
     }
 }
